8ece8d1938a7e32a37bb2c65502a8c9990aecd91
WangSimiao2000 <1872557359@qq.com>
2024-10-27 08:02:15 +0800

    Upload .clang-format File

--- Interview And CodingTest/Interview and Practice/Fun Plus/BinarySearch.cpp
+++ Interview And CodingTest/Interview and Practice/Fun Plus/BinarySearch.cpp
@@ -1,26 +1,32 @@
 #include <vector>
 using namespace std;
 
-int prize(vector<int>& res, int target) {
-    int left = 0;
+int prize(vector<int>& res, int target)
+{
+    int left  = 0;
     int right = res.size() - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;  // 避免溢出
-        if (res[mid] >= target) {
-            right = mid - 1;  // 尝试在左半边找更早的时间
-        } else {
-            left = mid + 1;   // 在右半边继续查找
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2; // 避免溢出
+        if (res[mid] >= target)
+        {
+            right = mid - 1; // 尝试在左半边找更早的时间
+        }
+        else
+        {
+            left = mid + 1; // 在右半边继续查找
         }
     }
-    
+
     // left指向第一个大于等于target的位置
     return (left < res.size() && res[left] >= target) ? left + 1 : -1; // 加1以返回1-based的时间
 }
 
-int main() {
-    vector<int> res = {1, 2, 3, 4, 5};
-    int target = 3;
-    int ans = prize(res, target);
+int main()
+{
+    vector<int> res    = {1, 2, 3, 4, 5};
+    int         target = 3;
+    int         ans    = prize(res, target);
     return 0;
 }
